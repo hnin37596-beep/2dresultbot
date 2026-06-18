28e58e77f53ebd45cfc4d9498f4e05de1d58198e
hnin37596-beep <hnin37596@gmail.com>
2026-06-18 14:17:26 +0630

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher, executor, types
+import requests
+
+bot = Bot(token='8779493234:AAGBv1c0_uT8exSlHg4jk5fM-0N63LKHdIY')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'data'])
+async def send_data(message: types.Message):
+    url = 'https://script.google.com/macros/s/AKfycbwYJTshdlnVtgxfOKEZPOxNtczkxalN2ivsf2AGHVBb_yzU-WEzPTx7llEiyXxKmldb/exec'
+    data = requests.get(url).json()
+    latest_data = data[-5:] # နောက်ဆုံး ၅ ခု
+    msg = "📊 ၂D ရလဒ်များ:\n\n"
+    for item in latest_data:
+        msg += f"🗓 {item['date']} | ⏰ {item['time']}\n🔢 2D: *{item['2d']}*\n\n"
+    await message.answer(msg, parse_mode="Markdown")
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
